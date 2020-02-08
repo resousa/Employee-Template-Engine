e52abc01edfc2f4b29ed7620425df8b59d831bd2
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-02-07 23:28:06 -0800

    passing all tests

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -4,7 +4,7 @@ const Employee = require('../lib/Employee');
 
 test('Can instantiate Employee instance', () => {
   const employee = new Employee();
-  expect(typeof e).toBe('object');
+  expect(typeof employee).toBe('object');
 });
 
 test('Can set name via constructor arguments', () => {
